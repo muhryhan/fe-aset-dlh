caa19d9ff17837e8cb4e3c3a6a56da2d72b86f84
MRyhan <mhmdryhan02@gmail.com>
2025-08-03 20:50:04 +0800

    add button back to page login

--- src/pages/ScanQrCode/ScanPage.tsx
+++ src/pages/ScanQrCode/ScanPage.tsx
@@ -1,12 +1,17 @@
+import { Link } from "react-router-dom";
 import QrScanner from "../../components/scan/qrScanner";
 
 const ScanPage = () => {
   return (
     <div className="min-h-screen bg-gray-900 flex flex-col items-center justify-start py-10 px-4">
       <h1 className="text-2xl font-semibold text-white mb-6">Scan QR</h1>
+
       <div className="w-full max-w-md bg-gray-800 rounded-2xl shadow-lg p-6">
         <QrScanner />
       </div>
+      <Link to="/" className="mt-5 text-md text-blue-400 hover:text-blue-300">
+        Kembali ke halaman Login
+      </Link>
     </div>
   );
 };
