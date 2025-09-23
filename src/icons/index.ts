cfdcff9727e371d8e8bf404756425e4701ade7c7
MRyhan <mhmdryhan02@gmail.com>
2025-09-23 11:57:07 +0800

    fix bug 5

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -64,7 +64,7 @@ import { ReactComponent as PlantIcon } from "./plant.svg?react";
 import { ReactComponent as ParkIcon } from "./park.svg?react";
 import { ReactComponent as BurialIcon } from "./burial.svg?react";
 import { ReactComponent as QRIcon } from "./qr.svg?react";
-import { ReactComponent as TriCycleIcon } from "./triCycle.svg?react";
+import { ReactComponent as TriCycleIcon } from "./tricycle.svg?react";
 import { ReactComponent as LandIcon } from "./land.svg?react";
 
 export {
